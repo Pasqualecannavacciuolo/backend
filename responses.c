50d1bc09933d940bde23d533e934a861624c6342
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-03-05 15:21:09 +0100

    Rimozine codice inutilizzato e modifica metodo format_http_response

--- responses.c
+++ responses.c
@@ -1,43 +1,7 @@
 #include "responses.h"
 
-char* send_get_response() {
-    char *message = NULL;
-    int message_size;
-    char message_size_str[20]; // Massimo numero di cifre per la dimensione sia 20
-
-    // Allocazione dinamica del messaggio
-    message = (char *)malloc(100 * sizeof(char)); // In base alle esigenze verra' regolata
-    if (message == NULL) {
-        printf("Errore durante l'allocazione della memoria per il messaggio.\n");
-        return NULL;
-    }
-    strcpy(message, "Hello, World!");
-    message_size = strlen(message);
-
-    // Converte l'intero message_size in una stringa
-    sprintf(message_size_str, "%d", message_size);
-
-    // Costruisce la stringa base
-    char *response = (char *)malloc(200 * sizeof(char)); // Allocazione dinamica della risposta
-    if (response == NULL) {
-        printf("Errore durante l'allocazione della memoria per la risposta.\n");
-        free(message);
-        return NULL;
-    }
-    strcpy(response, "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ");
-    strcat(response, message_size_str);
-    strcat(response, "\n\n");
-
-    // Concatena la stringa base con il messaggio
-    strcat(response, message);
-
-    free(message); // Libera la memoria allocata per il messaggio
-
-    return response;
-}
-
-char* format_http_response(char* jsonBody) {
-    size_t httpResponseSize = strlen("HTTP/1.1 200 OK\r\n") + strlen("Content-Type: application/json\r\n") +
+char* format_http_response(int status, char* jsonBody) {
+    size_t httpResponseSize = strlen("HTTP/1.1 XXX \r\n") + strlen("Content-Type: application/json\r\n") +
                               strlen("Content-Length: ") + strlen(jsonBody) + strlen("\r\n\r\n") + 1;
 
     char *httpResponse = (char *)malloc(httpResponseSize);
@@ -46,13 +10,16 @@ char* format_http_response(char* jsonBody) {
     }
 
     // Costruzione della risposta HTTP
-    strcpy(httpResponse, "HTTP/1.1 200 OK\r\n");
-    strcat(httpResponse, "Content-Type: application/json\r\n");
+    sprintf(httpResponse, "HTTP/1.1 %d ", status); // Inserisce lo status nella risposta
 
-    // Aggiungere la lunghezza del corpo della risposta
+    // Aggiungi il Content-Type
+    strcat(httpResponse, "\r\nContent-Type: application/json\r\n");
+
+    // Aggiungi la lunghezza del corpo della risposta
     sprintf(httpResponse + strlen(httpResponse), "Content-Length: %ld\r\n", strlen(jsonBody));
 
-    strcat(httpResponse, "\r\n"); // Fine delle intestazioni, inizio del corpo della risposta
+    // Aggiungi fine delle intestazioni e inizio del corpo della risposta
+    strcat(httpResponse, "\r\n");
     strcat(httpResponse, jsonBody);
 
     return httpResponse;
--- responses.h
+++ responses.h
@@ -6,8 +6,6 @@
 #ifndef RESPONSES_H
 #define RESPONSES_H
 
-// Prototipi delle funzioni
-char* send_get_response();
 
 /**
  * Formatta una risposta HTTP con il corpo JSON specificato.
@@ -15,10 +13,11 @@ char* send_get_response();
  * una risposta HTTP con intestazioni appropriate e il corpo JSON specificato.
  *
  * @param jsonBody Il corpo JSON da includere nella risposta HTTP.
+ * @param status Lo status della risposta HTTP.
  * @return Puntatore a una stringa allocata dinamicamente contenente la risposta HTTP formattata.
  *         Il chiamante è responsabile di liberare la memoria allocata.
  *         Se si verifica un errore durante l'allocazione di memoria, la funzione restituirà NULL.
  */
-char* format_http_response(char* jsonBody);
+char* format_http_response(int status, char* jsonBody);
 
 #endif
